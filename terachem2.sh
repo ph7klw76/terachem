029abdff1e94891d36a9d7f948ed3cebc9f99ae3
Kai Lin Woon <55481379+ph7klw76@users.noreply.github.com>
2022-03-09 13:19:00 +0800

    Create terachem2.sh

---
+++ terachem2.sh
@@ -0,0 +1,8 @@
+# Define TeraChem variables
+export TeraChem=/home/user/woon/terachem-1.93p
+export PATH=$TeraChem/bin:$PATH
+export LD_LIBRARY_PATH=$TeraChem/lib:$LD_LIBRARY_PATH
+export NBOEXE=$TeraChem/bin/nbo6.i4.exe
+
+# Run TeraChem
+
