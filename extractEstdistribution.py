26e0238a30a017cb3d07c44dc20fd9464bc5bbc9
Kai Lin Woon <55481379+ph7klw76@users.noreply.github.com>
2022-09-29 14:09:34 +0800

    Update extractEstdistribution.py

--- extractEstdistribution.py
+++ extractEstdistribution.py
@@ -1,9 +1,8 @@
-@author: ph7klw
-"""
-f=open('F:/LS-QMD/LS19/second/result.txt','r')
-f2=open('F:/LS-QMD/LS19/second/result2.txt','w')
+f=open('F:/LS-QMD/LS24/result.txt','r')
+f2=open('F:/LS-QMD/LS24/result2.txt','w')
 mydataS=[]
 mydataT=[]
+deltaE=0
 for i, line in enumerate(f):
     line=line.split()
     if int(line[1])==1:
@@ -13,21 +12,22 @@ for i, line in enumerate(f):
         a=int(line[7])
         b=int(line[9])
         E=float(line[3])
-        if i!=0:
-            for dataT in mydataT:
-                a1=dataT[0]
-                b1=dataT[1]
-                for dataS in mydataS:
-                    a2=dataS[0]
-                    b2=dataS[1]   
-                    if a1==a2 and b1==b2:
-                        deltaE=dataS[2]-dataT[2]
-                        f2.write(str(fff)+','+str(a1)+'->'+str(b1)+','+str(deltaE)+','+str(dataS[2])+'\n')
+    if i!=0:
+        for dataT in mydataT:
+            a1=dataT[0]
+            b1=dataT[1]
+            for dataS in mydataS:
+                a2=dataS[0]
+                b2=dataS[1]   
+                if a1==a2 and b1==b2:
+                    deltaE=dataS[2]-dataT[2]
+                    print(deltaE)
+                    f2.write(str(fff)+','+str(a1)+'->'+str(b1)+','+str(deltaE)+','+str(dataS[2])+'\n')
         mydataS,mydataT=[],[]
-        if ST<0.05:
-            mydataS=[(a,b,E)]
-        if ST>1.95:
-            mydataT=[(a,b,E)]
+    if ST<0.05:
+        mydataS=[(a,b,E)]
+    if ST>1.95:
+        mydataT=[(a,b,E)]
     if int(line[1])!=1:
         ST=float(line[5])
         a=int(line[7])
